cd0a26c81b9f1cbaf8549dc5ca590e4e1131eb9c
skinba <55181600+skinba@users.noreply.github.com>
2019-10-03 16:13:08 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -44,12 +44,15 @@ app.use((req, res, next) => {
 });
 
 app.use(function(req, res, next) {
-    //set headers to allow cross origin request.
-    res.setHeader('Access-Control-Allow-Origin: http://localhost:8080');
-    res.setHeader('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers : Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    next();
-  });
+  //set headers to allow cross origin request.
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // app.use(
 //     '/api-docs',
 //     swaggerUi.serve,

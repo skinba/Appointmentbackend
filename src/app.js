6d7d3d4b81c536fa3396571b1ce742ca476b56bd
skinba <55181600+skinba@users.noreply.github.com>
2019-10-04 13:39:51 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 app.use(function(req, res, next) {
   //set headers to allow cross origin request.
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  // res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
